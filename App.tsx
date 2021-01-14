99f933428bd21d141d9ef7910441d6297cc5ca8e
Rafa <rafa_zkeleton@hotmail.com>
2021-01-14 12:52:51 -0600

    Funcionamiento correcto de amcharts

--- App.tsx
+++ App.tsx
@@ -1,21 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Sales} from './Sales'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+  export default class App extends React.Component {
+
+    render() {
+      return (
+          <View style={styles.container}>
+            <Sales />
+            <StatusBar style="auto"/>
+          </View>
+      );
+    }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontFamily: 'sans-serif',
+      textAlign: 'center',
   },
 });
